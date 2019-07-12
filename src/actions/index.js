c9da93682e293112e6dfa9b0005ea22f1204ab54
Arthur <a.dhuy@orange.fr>
2019-07-12 23:50:38 +0200

    debug fetch post on load page

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,17 +4,6 @@ export const FETCH_POST = "FETCH_POST";
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
 const API_KEY = '123'
 
-export function fetchPost(id) {
-  return fetch(`${ROOT_URL}/${id}?key=${API_KEY}`)
-    .then(response => response.json())
-    .then((data) => {
-      return {
-        type: FETCH_POST,
-        payload: data
-      }
-  });
-}
-
 export function fetchPosts() {
   return fetch(`${ROOT_URL}?key=${API_KEY}`)
     .then(response => response.json())
@@ -26,4 +15,14 @@ export function fetchPosts() {
   });
 }
 
+export function fetchPost(id) {
+  return fetch(`${ROOT_URL}/${id}?key=${API_KEY}`)
+    .then(response => response.json())
+    .then((data) => {
+      return {
+        type: FETCH_POST,
+        payload: data
+      }
+  });
+}
 
