35fc5937bf1455bfa49cece61ca43bc4e214c8c4
Arthur <a.dhuy@orange.fr>
2019-07-12 13:27:19 +0200

    add index route

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,19 @@ import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
+// Assets //
 import '../assets/stylesheets/application.scss';
 
+// Reducers //
+import postsReducer from './reducers/posts_reducer.js';
+
 const reducers = combineReducers({
-  // key: reducer
+  posts: postsReducer
 });
+
+// Containers //
+import PostsIndex from './containers/posts_index.jsx';
+
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
@@ -19,9 +27,11 @@ const middlewares = applyMiddleware(reduxPromise, logger);
 ReactDOM.render(
   <Provider store={createStore(reducers, {}, middlewares)}>
     <Router history={history}>
-      <Switch>
-        TODO
-      </Switch>
+      <div className="thin-container">
+        <Switch>
+          <Route path="/" exact component={PostsIndex} />
+        </Switch>
+      </div>
     </Router>
   </Provider>,
   document.getElementById('root')
